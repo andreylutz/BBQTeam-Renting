64caf4e07bf84f2de12b367be115fdf7aec434f6
Anton Mushnin <anton.mushnin@gmail.com>
2022-05-27 15:53:05 +0300

    proplist layout

--- views/EditPropertyPage.jsx
+++ views/EditPropertyPage.jsx
@@ -5,33 +5,39 @@ const Layout = require('./Layout');
 function EditPropertyPage({ property, propertyTypes, user }) {
   return (
     <Layout>
-      <form name="edit_property" className="col-4 ms-5 mt-5" method="POST" action="/admin/properties/">
-        <div className="mb-3">
-          <textarea name="desc" rows="4" value={property.description} className="overflow-auto form-control" id="description"></textarea>
-        </div>
-        <input name="propId" type="hidden" value={property.id} />
-        <div className="input-group mb-3">
-          <label className="input-group-text" htmlFor="inputGroupSelect01">Тип жилья</label>
+      <div className="row">
+        <div className="col mx-auto">
+          <form name="edit_property" className="col-8 ms-5 mt-5" method="POST" action="/admin/properties/">
+            <div className="mb-3">
+              <textarea name="desc" rows="4" value={property.description} className="overflow-auto form-control" id="description"></textarea>
+            </div>
+            <input name="propId" type="hidden" value={property.id} />
+            <div className="input-group mb-3">
+              <label className="input-group-text" htmlFor="inputGroupSelect01">Тип жилья</label>
 
-          <select className="form-select" id="inputGroupSelect01" name="propType">
-            <option selected>{property.PropertyType.name}</option>
-            {
-              propertyTypes.map((type) => <option value={type.id}>{type.name}</option>)
-            }
-          </select>
-          <div className="input-group mb-3 mt-3">
-            <span className="input-group-text">&#x20bd;</span>
-            <span className="input-group-text">0</span>
-            <input type="text" name="price" value={property.rentalPrice} className="form-control" aria-label="Цена в рублях за месяц" />
-          </div>
-          <img src={property.photo} className="col-9 img-fluid img-rounded mx-auto" alt="..." />
-          <div className="input-group mb-3 mt-3">
-            <span className="input-group-text">http://</span>
-            <input name="photo" value={property.photo} type="text" className="form-control" aria-label="Адрес изображения" />
-          </div>
+              <select className="form-select" id="inputGroupSelect01" name="propType">
+                <option selected>{property.PropertyType.name}</option>
+                {
+                propertyTypes.map((type) => <option value={type.id}>{type.name}</option>)
+              }
+              </select>
+              <div className="input-group mb-3 mt-3">
+                <span className="input-group-text">&#x20bd;</span>
+                <span className="input-group-text">0</span>
+                <input type="text" name="price" value={property.rentalPrice} className="form-control" aria-label="Цена в рублях за месяц" />
+              </div>
+              <div className="input-group mb-3 mt-3">
+                <span className="input-group-text">http://</span>
+                <input name="photo" value={property.photo} type="text" className="form-control" aria-label="Адрес изображения" />
+              </div>
+            </div>
+            <button type="submit" className="btn btn-success mb-5">Сохранить</button>
+          </form>
         </div>
-        <button type="submit" className="btn btn-success mb-5">Сохранить</button>
-      </form>
+        <div className="col">
+          <img src={property.photo} className="col-9 img-fluid img-rounded mx-auto" alt="..." />
+        </div>
+      </div>
     </Layout>
   );
 }
