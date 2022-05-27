3222bba40ea8f284a4cbfdd4eab61b078b752cda
Anton Mushnin <anton.mushnin@gmail.com>
2022-05-27 10:20:38 +0300

    fixing paths

--- views/PropListPage.jsx
+++ views/PropListPage.jsx
@@ -1,13 +1,13 @@
 const React = require('react');
-const Layout = require('../views/Layout');
-const PropList = require('../views/PropList');
+const Layout = require('./Layout');
+const PropList = require('./PropList');
 
-function PropListPage({properties, user}) {
+function PropListPage({ properties, user }) {
   return (
     <Layout>
       <PropList items={properties} />
     </Layout>
-  )
+  );
 }
 
 module.exports = PropListPage;
