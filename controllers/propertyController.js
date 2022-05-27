5f6c7f3f94042ad9b5b92f7f2b7b8ce72511e334
Anton Mushnin <anton.mushnin@gmail.com>
2022-05-27 10:19:48 +0300

    include property types

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -4,7 +4,10 @@ const PropListPage = require('../views/PropListPage');
 async function getAllProperties(req, res) {
   let properties;
   try {
-    properties = await Property.findAll();
+    properties = await Property.findAll(
+      { include: [Property.Type] },
+    );
+    console.log(properties[0].PropertyType);
   } catch (error) {
     res.status(500);
     res.json({ error: error.message });
